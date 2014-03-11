dcb49c1062938a739cc69e22f9e400514ae58061
Kyle Estes <kyle.estes@primordial.com>
2014-03-11 09:11:41 -0500

    Formatted source.

--- src/kyleestes/Scat.java
+++ src/kyleestes/Scat.java
@@ -13,26 +13,27 @@ public class Scat extends AdvancedRobot {
 	}
 
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Stay at right angles to the opponent
+		// Stay at right angles to the opponent.
 		setTurnRight(e.getBearing() + 90 - 30 * movementDirection);
 
-		// If the bot has small energy drop,
-		// assume it fired
+		// If the bot has small energy drop, assume it fired.
 		double changeInEnergy = previousEnergy - e.getEnergy();
+		
 		if (changeInEnergy > 0 && changeInEnergy <= 3) {
 			// Dodge!
 			movementDirection = -movementDirection;
+			
 			setAhead((e.getDistance() / 4 + 25) * movementDirection);
 		}
-		// When a bot is spotted,
-		// sweep the gun and radar
+		
+		// When a bot is spotted, sweep the gun and radar.
 		gunDirection = -gunDirection;
 		setTurnGunRight(99999 * gunDirection);
 
-		// Fire directly at target
+		// Fire directly at target.
 		fire(2);
 
-		// Track the energy level
+		// Track the energy level.
 		previousEnergy = e.getEnergy();
 	}
 }
